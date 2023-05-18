9b1ec5e514236e43437a7147e1cf796063b31d9f
pvwnthem <pvwn.them@gmail.com>
2023-05-18 18:32:48 -0400

    start

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+import matplotlib as mp
+
+
+# Print Version Numbers of Used Libraries
+print("Python Version: %s" % sys.version)
+print("Numpy Version %s" % np.version.version)
+print("Matplotlib Version %s" %  mp.__version__)
